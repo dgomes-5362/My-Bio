2d9e815a852d27ef1dacfefea50fc95e5dac08a6
dgomes-5362 <dgomes5362@franklincummings.edu>
2025-02-21 11:48:36 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>This is my Bio Page</h1>
+      <h2 className="heading" contentEditable="true" spellCheck="false"></h2>
+      Derek Gomes
+      <img
+        src="https://www.empoweredpc.com/wp-content/uploads/2024/03/Panorama_angle_aqua_1200.png"
+        alt=""
+      />
+    </div>
+  );
+}
